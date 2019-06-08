6aa45645a1346403661469dd1296bd25e58e3a15
Jakub Filipiak <kuba.filipiak52@gmail.com>
2019-06-08 18:21:42 +0200

    Trials and errors

---
+++ src/main/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/models/Coordinates.java
@@ -0,0 +1,7 @@
+package jakubfilipiak.ForbiddenZonesGeneratorWeb.models;
+
+/**
+ * Created by Jakub Filipiak on 04.06.2019.
+ */
+public class Coordinates {
+}
---
+++ src/main/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/services/CoordinatesService.java
@@ -0,0 +1,7 @@
+package jakubfilipiak.ForbiddenZonesGeneratorWeb.services;
+
+/**
+ * Created by Jakub Filipiak on 04.06.2019.
+ */
+public class CoordinatesService {
+}
