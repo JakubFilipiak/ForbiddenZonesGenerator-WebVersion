9836d84cb3b2a20ad0e44ea04f210f20d6d1e8b8
Jakub Filipiak <kuba.filipiak52@gmail.com>
2019-07-02 23:06:58 +0200

    Remove deprecated imports and comments from main application

--- src/main/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/ForbiddenZonesGeneratorWebApplication.java
+++ src/main/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/ForbiddenZonesGeneratorWebApplication.java
@@ -1,7 +1,5 @@
 package jakubfilipiak.ForbiddenZonesGeneratorWeb;
 
-import jakubfilipiak.ForbiddenZonesGeneratorWeb.apis.ProcessingApi;
-import jakubfilipiak.ForbiddenZonesGeneratorWeb.models.Track;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,10 +9,5 @@ public class ForbiddenZonesGeneratorWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForbiddenZonesGeneratorWebApplication.class, args);
 
-//		ProcessingApi processingApi = new ProcessingApi();
-//
-//		Track track = new Track("D:/ForbiddenZonesGenerator " +
-//				"resources/LU-53-SP-ABM.trk");
-//		processingApi.processTrack(track);
 	}
 }
