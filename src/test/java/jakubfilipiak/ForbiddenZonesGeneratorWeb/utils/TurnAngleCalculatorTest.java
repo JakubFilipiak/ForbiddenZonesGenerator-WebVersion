f3785ccdcf13ec2498f7e5c8140f881b9e734a29
Jakub Filipiak <kuba.filipiak52@gmail.com>
2019-07-04 13:23:36 +0200

    Remove TurnAngleCalculatorTest to the correct package

--- src/test/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/utils/TurnAngleCalculatorTest.java
+++ src/test/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/utils/TurnAngleCalculatorTest.java
@@ -1,4 +1,4 @@
-package jakubfilipiak.ForbiddenZonesGeneratorWeb.services;
+package jakubfilipiak.ForbiddenZonesGeneratorWeb.utils;
 
 import jakubfilipiak.ForbiddenZonesGeneratorWeb.models.helpers.PointOfTrack;
 import jakubfilipiak.ForbiddenZonesGeneratorWeb.utils.TurnAngleCalculator;
