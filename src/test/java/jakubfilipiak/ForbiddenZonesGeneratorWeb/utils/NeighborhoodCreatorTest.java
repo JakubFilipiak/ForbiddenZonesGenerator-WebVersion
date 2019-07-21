e6c9cb2d310baf154b1b3fcb897c01f665471195
Jakub Filipiak <kuba.filipiak52@gmail.com>
2019-07-21 10:17:04 +0200

    Add one unit test for NeighborhoodCreatorTest (for radius=3)

--- src/test/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/utils/NeighborhoodCreatorTest.java
+++ src/test/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/utils/NeighborhoodCreatorTest.java
@@ -60,8 +60,10 @@ public class NeighborhoodCreatorTest {
                 new Coordinates(999, 1001),
                 new Coordinates(1000, 1001),
                 new Coordinates(1001, 1001),
+
                 new Coordinates(999, 1000),
                 new Coordinates(1001, 1000),
+
                 new Coordinates(999, 999),
                 new Coordinates(1000, 999),
                 new Coordinates(1001, 999)
@@ -73,6 +75,77 @@ public class NeighborhoodCreatorTest {
 
         // then
         assertThat(resultNeighbors.size()).isEqualTo(8);
+        assertThat(resultNeighbors).containsExactlyInAnyOrderElementsOf(expectedNeighbors);
+    }
+
+    @Test
+    public void shouldCorrectReturnNeighborsWhenRadiusIsThree() {
+        // given
+        Coordinates baseCoordinates = new Coordinates(1000, 1000);
+        int radiusOfPixels = 3;
+        List<Coordinates> expectedNeighbors = new ArrayList<>(Arrays.asList(
+                new Coordinates(997, 1003),
+                new Coordinates(998, 1003),
+                new Coordinates(999, 1003),
+                new Coordinates(1000, 1003),
+                new Coordinates(1001, 1003),
+                new Coordinates(1002, 1003),
+                new Coordinates(1003, 1003),
+
+                new Coordinates(997, 1002),
+                new Coordinates(998, 1002),
+                new Coordinates(999, 1002),
+                new Coordinates(1000, 1002),
+                new Coordinates(1001, 1002),
+                new Coordinates(1002, 1002),
+                new Coordinates(1003, 1002),
+
+                new Coordinates(997, 1001),
+                new Coordinates(998, 1001),
+                new Coordinates(999, 1001),
+                new Coordinates(1000, 1001),
+                new Coordinates(1001, 1001),
+                new Coordinates(1002, 1001),
+                new Coordinates(1003, 1001),
+
+                new Coordinates(997, 1000),
+                new Coordinates(998, 1000),
+                new Coordinates(999, 1000),
+                new Coordinates(1001, 1000),
+                new Coordinates(1002, 1000),
+                new Coordinates(1003, 1000),
+
+                new Coordinates(997, 999),
+                new Coordinates(998, 999),
+                new Coordinates(999, 999),
+                new Coordinates(1000, 999),
+                new Coordinates(1001, 999),
+                new Coordinates(1002, 999),
+                new Coordinates(1003, 999),
+
+                new Coordinates(997, 998),
+                new Coordinates(998, 998),
+                new Coordinates(999, 998),
+                new Coordinates(1000, 998),
+                new Coordinates(1001, 998),
+                new Coordinates(1002, 998),
+                new Coordinates(1003, 998),
+
+                new Coordinates(997, 997),
+                new Coordinates(998, 997),
+                new Coordinates(999, 997),
+                new Coordinates(1000, 997),
+                new Coordinates(1001, 997),
+                new Coordinates(1002, 997),
+                new Coordinates(1003, 997)
+        ));
+
+        // when
+        List<Coordinates> resultNeighbors =
+                neighborhoodCreator.getPixelNeighbors(baseCoordinates, radiusOfPixels);
+
+        // then
+        assertThat(resultNeighbors.size()).isEqualTo(48);
         assertThat(resultNeighbors).containsExactlyInAnyOrderElementsOf(expectedNeighbors);
     }
 }
