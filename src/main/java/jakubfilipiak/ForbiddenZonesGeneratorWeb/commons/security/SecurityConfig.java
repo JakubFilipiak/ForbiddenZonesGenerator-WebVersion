18c88b0d9be36cd5ec1e8f5466fc1760279fcc3e
Jakub Filipiak <kuba.filipiak52@gmail.com>
2019-07-22 16:26:45 +0200

    Add Spring Security with basic in memory authentication

---
+++ src/main/java/jakubfilipiak/ForbiddenZonesGeneratorWeb/commons/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package jakubfilipiak.ForbiddenZonesGeneratorWeb.commons.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by Jakub Filipiak on 22.07.2019.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/newtrack")
+                .and()
+                .logout()
+                .logoutUrl("/logout");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
